daadcc9c35fe81d7af10f4a9232ac364e6f09b9e
Sabando Gonzalez <lolia_15@hotmail.com>
2018-10-28 01:42:41 -0300

    tooltip + diseño

--- src/js/App.jsx
+++ src/js/App.jsx
@@ -7,17 +7,30 @@ class App extends Component {
     this.state={
       page: 0,
       images:[],
+      loading: true,
+    }
+
+    window.onscroll=()=>{
+      if(this.state.loading === true){
+        return
+      }if(
+        window.innerHeight + document.documentElement.scrollTop === document.documentElement.offsetHeight
+      ){
+        this.loadImages();
+      }
     }
   }
 
   loadImages() {   
     let newPage= this.state.page+1; 
     let firstImages=this.state.images;
-    fetch(`https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=0dc9484b76324cd873e335b941191c0f&tags=flowers&format=json&nojsoncallback=1&page=${newPage}`)
+    fetch(`https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=0dc9484b76324cd873e335b941191c0f&tags=flowers&format=json&nojsoncallback=1&page=${newPage}&per_page=20`)
     .then(response=>response.json())
     .then(data =>{
       this.setState({
-        images: firstImages.concat(data.photos.photo)
+        images: firstImages.concat(data.photos.photo),
+        page: newPage,
+        loading: false
       })
     })
   }
@@ -25,7 +38,6 @@ class App extends Component {
   componentDidMount(){
     this.loadImages()
   }
-
 
   render() {
     return (
--- src/js/components/Imagen/Imagen.jsx
+++ src/js/components/Imagen/Imagen.jsx
@@ -1,23 +1,47 @@
 import React, { Component } from "react";
+import ReactTooltip from "react-tooltip";
+import { findDOMNode } from "react-dom";
 import "./Imagen.css";
 
 
-class Imagen extends Component{
-  constructor(){
-    super()
+class Imagen extends Component {
+  constructor(props) {
+    super(props)
+    this.state={
+      data: {},
+      loading: true
+    }
   }
 
-  render(){
+  infoTooltip() {   
+    fetch(`https://api.flickr.com/services/rest/?method=flickr.photos.getinfo&api_key=0dc9484b76324cd873e335b941191c0f&photo_id=45589503031&secret=92ad3c3d65470592&tags=flowers&format=json&nojsoncallback=1`)
+    .then(response=>response.json())
+    .then(data =>{
+      this.setState({
+        data: data.photo,
+        loading:false
+      })
+    })
+  }
+
+  componentWillMount(){
+    this.infoTooltip()
+  }
+
+  render() {
     let image = this.props.data;
-    return(
+    let title= this.props.data.title;
+    return (
       <div className="imagen">
-        <img className="image" src={`https://farm${image.farm}.staticflickr.com/${image.server}/${image.id}_${image.secret}.jpg`} alt="images"/>
-        <p>Título: </p>
+        <img className="image" onClick={()=>{ ReactTooltip.show(findDOMNode(this.refs.foo))}} src={`https://farm${image.farm}.staticflickr.com/${image.server}/${image.id}_${image.secret}.jpg`} alt="images" />
+        <p ref="foo" data-tip={title}></p>
+        {/* <p>Título: </p>
         <p>Descripción:</p>
         <p>Fecha de Post:</p>
         <p>Fecha de actualización</p>
         <p>Tags:</p>
-        <p>Usuario:</p>
+        <p>Usuario:</p> */}
+        <ReactTooltip />
       </div>
     )
   }
